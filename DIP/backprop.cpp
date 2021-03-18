6f0c347be41e81b21fee1d5a878c64c6f66eaeb1
zur0037 <zur0037@vsb.cz>
2021-03-18 18:39:32 +0100

    backprop methods

--- DIP/backprop.cpp
+++ DIP/backprop.cpp
@@ -10,7 +10,13 @@
 #define ETA 0.1
 
 #define SQR( x ) ( ( x ) * ( x ) )
+const double sigmoid(const double sij, const double lambda) {
+	return 1.0 / (1.0 + exp(-1 * LAMBDA * sij));
+}
 
+const double derivate_sigmoid(const double y) {
+	return y * (1 - y);
+}
 void randomize(double* p, int n)
 {
 	for (int i = 0; i < n; i++) {
@@ -24,40 +30,37 @@ NN* createNN(int n, int h, int o)
 	NN* nn = new NN;
 
 	nn->n = new int[3];
-	nn->n[0] = n;
-	nn->n[1] = h;
-	nn->n[2] = o;
-	nn->l = 3;
+	nn->n[0] = n; // pocet neuronu ve vstupni vrstve
+	nn->n[1] = h; // pocet neuronu v hidden vrstve
+	nn->n[2] = o; // pocet neuronu v output vrstve
+	nn->l = 3; // pocet vrstev
 
 	nn->w = new double** [nn->l - 1];
 
+	for (int layerNum = 0; layerNum < nn->l - 1; layerNum++) {
+		nn->w[layerNum] = new double* [nn->n[layerNum + 1]]; // pole pro vahy vrstev
+		for (int j = 0; j < nn->n[layerNum + 1]; j++) {
+			nn->w[layerNum][j] = new double[nn->n[layerNum]]; // pole vah pro vrstvu
+			randomize(nn->w[layerNum][j], nn->n[layerNum]); // naplneni
 
-	for (int k = 0; k < nn->l - 1; k++)
-	{
-		nn->w[k] = new double* [nn->n[k + 1]];
-		for (int j = 0; j < nn->n[k + 1]; j++)
-		{
-			nn->w[k][j] = new double[nn->n[k]];
-			randomize(nn->w[k][j], nn->n[k]);
-			// BIAS
-			//nn->w[k][j] = new double[nn->n[k] + 1];			
-			//randomize( nn->w[k][j], nn->n[k] + 1 );
+			//nn->w[layerNum][j] = new double[nn->n[layerNum] + 1]; // BIAS	
+			//randomize( nn->w[layerNum][j], nn->n[layerNum] + 1 ); // naplneni
 		}
 	}
 
-	nn->y = new double* [nn->l];
+	nn->y = new double* [nn->l]; // pole pro vystupni vyktory
 	for (int k = 0; k < nn->l; k++) {
-		nn->y[k] = new double[nn->n[k]];
-		memset(nn->y[k], 0, sizeof(double) * nn->n[k]);
+		nn->y[k] = new double[nn->n[k]]; // nodes pro vystupni vektor
+		memset(nn->y[k], 0, sizeof(double) * nn->n[k]); // naplneni nulami
 	}
 
-	nn->in = nn->y[0];
-	nn->out = nn->y[nn->l - 1];
+	nn->in = nn->y[0]; //vstupni vektor = vystupni vektory[0]
+	nn->out = nn->y[nn->l - 1]; // vystupni vektor = posledni z vystupnich vektoru
 
-	nn->d = new double* [nn->l];
+	nn->d = new double* [nn->l]; // pole pro chyby vrstev
 	for (int k = 0; k < nn->l; k++) {
-		nn->d[k] = new double[nn->n[k]];
-		memset(nn->d[k], 0, sizeof(double) * nn->n[k]);
+		nn->d[k] = new double[nn->n[k]]; // neurony chyb nejake vrstvy
+		memset(nn->d[k], 0, sizeof(double) * nn->n[k]); // naplneni nulami
 	}
 
 	return nn;
@@ -92,14 +95,51 @@ void releaseNN(NN*& nn)
 
 void feedforward(NN* nn)
 {
-	//TODO
+	double ski = 0;
+	for (int level = 1; level < nn->l; level++) {
+		for (int node = 0; node < nn->n[level]; node++) {
+			ski = 0;
+			for (int neighbourNode = 0; neighbourNode < nn->n[level - 1]; ++neighbourNode) {
+				ski += nn->y[level - 1][neighbourNode] * nn->w[level - 1][node][neighbourNode];
+			}
+			nn->y[level][node] = sigmoid(ski, LAMBDA);
+		}
+	}
 }
 
 double backpropagation(NN* nn, double* t)
 {
 	double error = 0.0;
+	const int outputLayerIndex = nn->l - 1;
+	double y_kj = 0;
 
-	//TODO
+	for (int i = 0; i < nn->n[outputLayerIndex]; i++) {
+		y_kj = derivate_sigmoid(nn->y[outputLayerIndex][i]);
+		nn->d[outputLayerIndex][i] = (t[i] - nn->y[outputLayerIndex][i]) * LAMBDA * y_kj;
+	}
+
+	for (int k = nn->l - 2; k > 0; k--) {
+		for (int i = 0; i < nn->n[k]; i++) {
+			nn->d[k][i] = 0.0;
+			for (int j = 0; j < nn->n[k + 1]; j++) {
+				nn->d[k][i] += nn->d[k + 1][j] * nn->w[k][j][i];
+			}
+			nn->d[k][i] *= LAMBDA * derivate_sigmoid(nn->y[k][i]);
+		}
+	}
+
+	for (int k = 0; k < nn->l - 1; k++) {
+		for (int i = 0; i < nn->n[k + 1]; i++) {
+			for (int j = 0; j < nn->n[k]; j++) {
+				nn->w[k][i][j] += ETA * nn->d[k + 1][i] * nn->y[k][j];
+			}
+		}
+	}
+
+	for (int i = 0; i < nn->n[nn->l - 1]; i++) {
+		error += pow(t[i] - nn->y[2][i], 2);
+	}
+	error /= 2.0;
 
 	return error;
 }
@@ -134,6 +174,6 @@ int getOutput(NN* nn, bool verbose)
 		}
 	}
 	if (verbose) printf(" -> %d\n", max_i);
-	if (nn->out[0] > nn->out[1] && nn->out[0] - nn->out[1] < 0.1) return 2;
+	//if (nn->out[0] > nn->out[1] && nn->out[0] - nn->out[1] < 0.1) return 2;
 	return max_i;
 }
